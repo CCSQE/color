6f68b77fdfcc7c3ee6413bd9cbab5791c0864c44
anshu <aayushjain7897@gmail.com>
2019-12-29 22:38:49 +0530

    Add files via upload
    upload successfully

---
+++ app.js
@@ -0,0 +1,8 @@
+function bgchanger() {
+  if (this.scrollY > this.innerHeight / 1.5) {
+    document.body.classList.add("bg-active");
+  } else {
+    document.body.classList.remove("bg-active");
+  }
+}
+window.addEventListener("scroll", bgchanger);
